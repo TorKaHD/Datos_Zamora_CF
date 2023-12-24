e48bcf351e8032eda114406ed223abcda40363a8
TorKaHD <31444933+TorKaHD@users.noreply.github.com>
2023-12-24 11:12:05 +0100

    Página Web Resultados Zamora C.F - versión 2

--- DatosWebZamora_v2/javascript_json_pretemporada_23_24_v2.js
+++ DatosWebZamora_v2/javascript_json_pretemporada_23_24_v2.js
@@ -293,29 +293,29 @@ document.addEventListener("DOMContentLoaded", function () {
 
     function mostrarPartido(partido) {
         const div = document.createElement("div");
-
+    
         div.innerHTML = `
-            <header><h2 class = "p-1 sm:text-sm md:text-base lg:text-lg xl:text-xl text-yellow-950"><u>Partido ${partido.id}</u></h2></header>
+            <header><h2 class="p-1 sm:text-sm md:text-base lg:text-lg xl:text-xl text-yellow-950 text-left"><u>Partido ${partido.id}</u></h2></header>
             <section>
-            <p class = "p-1 sm:text-sm md:text-base lg:text-lg xl:text-xl text-yellow-950"><strong>Fecha: </strong>${partido.fecha}</p>
-            <p class = "p-1 sm:text-sm md:text-base lg:text-lg xl:text-xl text-yellow-950"><strong>Hora:</strong> ${partido.hora}</p>
-            <p class = "p-1 sm:text-sm md:text-base lg:text-lg xl:text-xl text-yellow-950"><strong>Equipos:</strong> ${partido.equipos.local} vs ${partido.equipos.visitante}</p>
-            <p class = "p-1 sm:text-sm md:text-base lg:text-lg xl:text-xl text-yellow-950"><strong>Estadio:</strong> ${partido.estadio}</p>
-            <p class = "p-1 sm:text-sm md:text-base lg:text-lg xl:text-xl text-yellow-950"><strong>Resultado:</strong> ${partido.resultado.local} - ${partido.resultado.visitante}</p>
-            <p class = "p-1 sm:text-sm md:text-base lg:text-lg xl:text-xl text-yellow-950"><strong>Goleadores Local:</strong> ${mostrarGoleadores(partido.goles.local)}</p>
-            <p class = "p-1 sm:text-sm md:text-base lg:text-lg xl:text-xl text-yellow-950"><strong>Goleadores Visitante:</strong> ${mostrarGoleadores(partido.goles.visitante)}</p>
-            <p class = "p-1 sm:text-sm md:text-base lg:text-lg xl:text-xl text-yellow-950"><strong>Asistencias Local:</strong> ${mostrarAsistencias(partido.asistencias.local)}</p>
-            <p class = "p-1 sm:text-sm md:text-base lg:text-lg xl:text-xl text-yellow-950"><strong>Asistencias Visitante:</strong> ${mostrarAsistencias(partido.asistencias.visitante)}</p>
+            <p class="p-1 sm:text-sm md:text-base lg:text-lg xl:text-xl text-yellow-950 text-left"><strong>Fecha: </strong>${partido.fecha}</p>
+            <p class="p-1 sm:text-sm md:text-base lg:text-lg xl:text-xl text-yellow-950 text-left"><strong>Hora:</strong> ${partido.hora}</p>
+            <p class="p-1 sm:text-sm md:text-base lg:text-lg xl:text-xl text-yellow-950 text-left"><strong>Equipos:</strong> ${partido.equipos.local} vs ${partido.equipos.visitante}</p>
+            <p class="p-1 sm:text-sm md:text-base lg:text-lg xl:text-xl text-yellow-950 text-left"><strong>Estadio:</strong> ${partido.estadio}</p>
+            <p class="p-1 sm:text-sm md:text-base lg:text-lg xl:text-xl text-yellow-950 text-left"><strong>Resultado:</strong> ${partido.resultado.local} - ${partido.resultado.visitante}</p>
+            <p class="p-1 sm:text-sm md:text-base lg:text-lg xl:text-xl text-yellow-950 text-left"><strong>Goleadores Local:</strong> ${mostrarGoleadores(partido.goles.local)}</p>
+            <p class="p-1 sm:text-sm md:text-base lg:text-lg xl:text-xl text-yellow-950 text-left"><strong>Goleadores Visitante:</strong> ${mostrarGoleadores(partido.goles.visitante)}</p>
+            <p class="p-1 sm:text-sm md:text-base lg:text-lg xl:text-xl text-yellow-950 text-left"><strong>Asistencias Local:</strong> ${mostrarAsistencias(partido.asistencias.local)}</p>
+            <p class="p-1 sm:text-sm md:text-base lg:text-lg xl:text-xl text-yellow-950 text-left"><strong>Asistencias Visitante:</strong> ${mostrarAsistencias(partido.asistencias.visitante)}</p>
             ${mostrarNotas(partido.notas)}
-            <p class = "p-1 sm:text-sm md:text-base lg:text-lg xl:text-xl text-yellow-950"><strong>----------</strong></p>
+            <p class="p-1 sm:text-sm md:text-base lg:text-lg xl:text-xl text-yellow-950 text-left"><strong>----------</strong></p>
             </section>
             <!-- Personaliza la visualización de otros datos aquí -->
-
+    
             <hr>
         `;
-
+    
         return div;
-    }
+    }    
 
     function mostrarGoleadores(goleadores) {
         if (goleadores.length === 0) {
@@ -337,8 +337,8 @@ document.addEventListener("DOMContentLoaded", function () {
 
     function mostrarNotas(notas) {
         if (notas && notas.length > 0) {
-            const notasHTML = notas.map(nota => `<p class = "p-1 sm:text-sm md:text-base lg:text-lg xl:text-xl" style = "color: #3b1419;">${nota}</p>`).join("");
-            return `<section><p class = "p-1 sm:text-sm md:text-base lg:text-lg xl:text-xl" style = "color: #3b1419;"><strong>Notas Adicionales:</strong></p>${notasHTML}</section>`;
+            const notasHTML = notas.map(nota => `<p class = "p-1 sm:text-sm md:text-base lg:text-lg xl:text-xl text-left" style = "color: #3b1419;">${nota}</p>`).join("");
+            return `<section><p class = "p-1 sm:text-sm md:text-base lg:text-lg xl:text-xl text-left" style = "color: #3b1419;"><strong>Notas Adicionales:</strong></p>${notasHTML}</section>`;
         } else {
             return "";
         }
